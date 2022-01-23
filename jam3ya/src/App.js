3c6efa4c876614c9863d4ab10793e468cb46654d
TaherNaqi <eng-naqi@hotmail.com>
2022-01-23 15:52:14 +0300

    Detail page

--- jam3ya/src/App.js
+++ jam3ya/src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import "./App.css";
 import Detail from "./Components/Detail";
-import Navbar from "./Components/Navbar";
 function App() {
   return (
     <div>
-      <Navbar />
       <Detail />
     </div>
   );
