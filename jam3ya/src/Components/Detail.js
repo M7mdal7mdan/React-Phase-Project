c98c561e253fd571cb3587cd6a9e20ed98734852
TaherNaqi <eng-naqi@hotmail.com>
2022-01-29 15:34:07 +0300

    Done Task

--- jam3ya/src/Components/Detail.js
+++ jam3ya/src/Components/Detail.js
@@ -1,80 +1,64 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import jam3yaStore from "../stores/jam3yaStore";
 import { Container, Card, Button } from "react-bootstrap";
 import { observer } from "mobx-react";
 import moment from "moment";
 import authStore from "../stores/authStore";
-import Swal from 'sweetalert2'
-import UpdateJam3yaModal from "./UpdateJam3yaModal"
+import Swal from "sweetalert2";
+import UpdateJam3yaModal from "./UpdateJam3yaModal";
 import { useNavigate } from "react-router-dom";
-
 
 function Detail() {
   const [isOpen, setIsOpen] = useState(false);
-    const closeModal = () => setIsOpen(false);
+  const closeModal = () => setIsOpen(false);
   const slug = useParams().slug;
   let navigate = useNavigate();
   if (jam3yaStore.loading) {
     return <h1>Loading</h1>;
   }
   let jam3ya = jam3yaStore.jam3yas.find((j) => j.slug === slug);
-  
+
   const { user } = authStore;
-  const userexist = user && jam3ya.users.some((u) => u._id === user._id); 
-   
-  
-    const handleDelete = () => {
-      jam3yaStore.deleteJam3ya(jam3ya._id);
-      Swal.fire(
-        `${jam3ya.title} Jam3ya has been deleted successfully `,
-        '',
-        'success'
-      )
-      navigate(`/profile`);
-    };
+  const userexist = user && jam3ya.users.some((u) => u._id === user._id);
+
+  const handleDelete = () => {
+    jam3yaStore.deleteJam3ya(jam3ya._id);
+    Swal.fire(
+      `${jam3ya.title} Jam3ya has been deleted successfully `,
+      "",
+      "success"
+    );
+    navigate(`/profile`);
+  };
   const handleJoin = () => {
-      jam3yaStore.joinJam3ya(user, jam3ya);
-      
-      Swal.fire(
-        `You have joined ${jam3ya.title}`,
-        '',
-        'success'
-      )
-    };
-    const handleLeave = () => {
-      jam3yaStore.leaveJam3ya(jam3ya._id);
-      Swal.fire(
-        'We are sorry to see you go',
-        '',
-        'success'
-      )
-    };
-    const startDate = moment(jam3ya.startDate).format("YYYY-MM-DD");
-    const endDate = moment(jam3ya.endDate).format("YYYY-MM-DD");
-    const checkJam3ya = () => {
-      if (jam3ya.startDate) {
-        if (new Date(jam3ya.startDate) > new Date()) return true;
-        return false;
-      }
-    };
-    const openModal = () => setIsOpen(true);
+    jam3yaStore.joinJam3ya(user, jam3ya);
+
+    Swal.fire(`You have joined ${jam3ya.title}`, "", "success");
+  };
+  const handleLeave = () => {
+    jam3yaStore.leaveJam3ya(jam3ya._id);
+    Swal.fire("We are sorry to see you go", "", "success");
+  };
+  const startDate = moment(jam3ya.startDate).format("YYYY-MM-DD");
+  const endDate = moment(jam3ya.endDate).format("YYYY-MM-DD");
+  const checkJam3ya = () => {
+    if (jam3ya.startDate) {
+      if (new Date(jam3ya.startDate) > new Date()) return true;
+      return false;
+    }
+  };
+  const openModal = () => setIsOpen(true);
   return (
-    <Container 
-      fluid
-    
-    >
-      <Card
-        className="d-flex align-items-center "
-       
-      >
+    <Container fluid>
+      <Card className="d-flex align-items-center ">
         <img className="img-detail" src={jam3ya.image} alt="jam3ya" />
 
-        <Card.Title  style={{ marginTop: "30px", fontWeight: "bold" }}>
+        <Card.Title style={{ marginTop: "30px", fontWeight: "bold" }}>
           <h4>Jam3ya Details</h4>
         </Card.Title>
 
-        <Card.Body >
+        <Card.Body>
           <Card.Title>Jam3ya title: {jam3ya.title}</Card.Title>
           <Card.Title>Jam3ya owner: {jam3ya.author.username}</Card.Title>
           <Card.Title>Start date: {startDate}</Card.Title>
@@ -87,45 +71,55 @@ function Detail() {
               : "full"}
           </Card.Title>
         </Card.Body>
-        <Card.Text>
-        Remaining spots:{" "}
-            {jam3ya.limit - jam3ya.users.length > 0
-              ? jam3ya.limit - jam3ya.users.length
-              : "full"}
-          </Card.Text>
 
-          {user && checkJam3ya() && (
-            <div className="searchBar">
-              {!userexist && jam3ya.users.length < jam3ya.limit ? (
-                <>
-                  <Button variant='secondary' className="delete  align-right" onClick={handleJoin}>
-                    Join
-                  </Button>
-                </>
-              ) : (
-                userexist && (
-                  <Button variant='secondary' className="delete  align-right mx-1" onClick={handleLeave}>
-                    Leave
-                  </Button>
-                )
-              )}
-              {jam3ya.author._id === user._id && (
-                <>
-                  <Button variant='secondary' className="delete  align-right mx-1" onClick={openModal}>
-                    Update
-                  </Button>
-                  <Button variant='secondary' className="delete  align-right mx-1" onClick={handleDelete}>
-                    Delete
-                  </Button>
-                </>
-              )}
-              <UpdateJam3yaModal
-            isOpen={isOpen}
-            closeModal={closeModal}
-            jam3ya={jam3ya}
-          />
-            </div>
-          )}
+        {user && checkJam3ya() && (
+          <div className="searchBar">
+            {!userexist && jam3ya.users.length < jam3ya.limit ? (
+              <>
+                <Button
+                  variant="secondary"
+                  className="delete  align-right"
+                  onClick={handleJoin}
+                >
+                  Join
+                </Button>
+              </>
+            ) : (
+              userexist && (
+                <Button
+                  variant="secondary"
+                  className="delete  align-right mx-1"
+                  onClick={handleLeave}
+                >
+                  Leave
+                </Button>
+              )
+            )}
+            {jam3ya.author._id === user._id && (
+              <>
+                <Button
+                  variant="secondary"
+                  className="delete  align-right mx-1"
+                  onClick={openModal}
+                >
+                  Update
+                </Button>
+                <Button
+                  variant="secondary"
+                  className="delete  align-right mx-1"
+                  onClick={handleDelete}
+                >
+                  Delete
+                </Button>
+              </>
+            )}
+            <UpdateJam3yaModal
+              isOpen={isOpen}
+              closeModal={closeModal}
+              jam3ya={jam3ya}
+            />
+          </div>
+        )}
       </Card>
     </Container>
   );
